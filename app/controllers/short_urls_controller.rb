f433b34bbfc624b315527166fcf431f280b4a5fc
Roberto Sequeira <roberto@robertosequeira.info>
2020-12-02 20:11:52 -0600

    Return top 100

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,9 +1,7 @@
 class ShortUrlsController < ApplicationController
 
-  # Since we're working on an API, we don't have authenticity tokens
-  skip_before_action :verify_authenticity_token
-
   def index
+    render json: { urls: ShortUrl.top }
   end
 
   def create
--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,6 +2,8 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
+  scope :top, ->(top = 100) { limit(top).order(click_count: :desc) }
+
   validate :validate_full_url
 
   def short_code
